bb1e1785daad4b58577be374cbc73340373ec95d
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-06-21 14:46:00 +0700

    Update bot8.php

--- bot8.php
+++ bot8.php
@@ -11,7 +11,7 @@ $_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow08;shbid=18600;shbt
 
 $xx = 0;
 while(true){
-	if($_time>6){
+	if($_time>0){
       $jumlah= "9";
         $_POST['tipe'] = "followers";
         $target = "1918078581";
